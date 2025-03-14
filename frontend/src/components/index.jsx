e4dc9db0883b9a59ec28d63e2ded566555b27d21
S17S17 <164513807+S17S17@users.noreply.github.com>
2025-03-15 00:55:35 +0100

    Add components index file for easy imports

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout';
+import ShoppingAssistant from './ShoppingAssistant';
+import BrowserSimulation from './BrowserSimulation';
+import AgentConsole from './AgentConsole';
+import ShoppingResults from './ShoppingResults';
+
+export {
+  Layout,
+  ShoppingAssistant,
+  BrowserSimulation,
+  AgentConsole,
+  ShoppingResults
+};
